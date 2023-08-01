88aeb9ef33092c0eb0609c6a978c3191f0b7a014
yomi <120027289+Jinmeishao@users.noreply.github.com>
2023-08-02 00:28:54 +0900

    20230802 initialize project Java 8 KDK 11.0.18 Spring Boot 2.7.14

---
+++ src/main/java/com/board/facamboard/FacamBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.facamboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacamBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacamBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/board/facamboard/FacamBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.facamboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacamBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
